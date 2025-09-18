1ba8ed9a9429c0e66667a0f8656451260ffde6ea
tofik <tofik.imscit21@gmail.com>
2025-09-18 17:10:34 +0530

    Database Changed

--- icon_invoice/settings.py
+++ icon_invoice/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'icon_invoice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'invoices_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Shaikh786@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
